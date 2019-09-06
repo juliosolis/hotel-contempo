66306eb45906c161686498ad0afcf34683b6894e
Julio César Solis Lainez <js@juliosolis.com>
2019-09-06 14:46:20 -0600

    Enable nearbybooking Create js file to store script

---
+++ js/nearby.js
@@ -0,0 +1,13 @@
+var __nby_opciones = __nby_opciones || {
+    cuenta: 2,
+    idioma: "en",
+    canal: 1
+};
+(function (d) {
+    var t = d.createElement("script");
+    t.type = "text/javascript";
+    t.async = true;
+    t.src = "https://cdn.nearbybooking.com/widget.js";
+    var s = d.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(t, s);
+})(document);
